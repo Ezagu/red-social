d139ed4bb6bdef36f0869e814a6624935552125a
Ezagu <agustincalpedm@gmail.com>
2026-01-27 16:14:07 -0300

    Modificacion en la conexion con mongodb

--- backend/arrancar-proyecto.bat
+++ backend/arrancar-proyecto.bat
@@ -1,2 +1,3 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\8.0\bin && mongod.exe"
 start cmd.exe /k "cd C:\Users\agust\OneDrive\Escritorio\Proyectos\red-social\backend && npm start"
+start cmd.exe /k "cd C:\Users\agust\OneDrive\Escritorio\Proyectos\red-social\frontend && npm run dev"
