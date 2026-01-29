6914e0e51721ff60d2302bcffd9a93096c6fd075
Ezagu <agustincalpedm@gmail.com>
2026-01-29 01:25:33 -0300

    transactions en comments

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -10,6 +10,8 @@ const create = async (req, res) => {
   const { publicationId, text } = req.body;
   const parentComment = req.body.parentComment || null;
 
+  const session = await mongoose.startSession();
+
   try {
     // Validar que la publicación exista
     const publication = await Publication.findById(publicationId);
@@ -21,107 +23,129 @@ const create = async (req, res) => {
       });
     }
 
-    // Crear comentario
-    const newComment = await Comment.create({
-      user,
-      publication: publicationId,
-      text,
-      parentComment,
-    });
+    let newComment;
 
-    await newComment.populate("user", "-email -password -role -__v");
-
-    // Determinar a quien notificar
-    let userToNotificate = publication.user._id;
-
-    // MODIFICAR CONTADORES
-    // Si tiene un comentario padre, aumentarle el contador de respuestas
-    if (parentComment) {
-      const parentComent = await Comment.findByIdAndUpdate(parentComment, {
-        $inc: { repliesCount: 1 },
+    await session.withTransaction(async () => {
+      // Crear comentario
+      newComment = new Comment({
+        user,
+        publication: publicationId,
+        text,
+        parentComment,
       });
-      userToNotificate = parentComent.user._id;
-    } else {
-      // Aumentar contador de comentarios de publicacion
-      await Publication.findByIdAndUpdate(publicationId, {
-        $inc: { commentsCount: 1 },
-      });
-    }
+      newComment.save({ session });
 
-    //NOTIFICACIÓN
-    // Generar notificacion si no es el mismo usuario el que se comenta
-    if (userToNotificate.toString() !== user.toString()) {
-      await Notification.create({
-        user: userToNotificate,
-        fromUser: user,
-        targetType: "Comment",
-        targetId: newComment._id,
-      });
+      await newComment.populate("user", "-email -password -role -__v");
 
-      await User.findByIdAndUpdate(userToNotificate, {
-        $inc: { unreadNotificationsCount: 1 },
-      });
-    }
+      // Determinar a quien notificar
+      let userToNotificate = publication.user._id;
 
-    return res.status(201).json({
-      status: "success",
-      message: "Comentario publicado",
-      comment: newComment,
+      // MODIFICAR CONTADORES
+      if (parentComment) {
+        // Si tiene un comentario padre, aumentarle el contador de respuestas
+        const parentComent = await Comment.findByIdAndUpdate(
+          parentComment,
+          {
+            $inc: { repliesCount: 1 },
+          },
+          { session },
+        );
+        userToNotificate = parentComent.user._id;
+      } else {
+        // Sino aumentar contador de comentarios de publicacion
+        await Publication.findByIdAndUpdate(
+          publicationId,
+          {
+            $inc: { commentsCount: 1 },
+          },
+          { session },
+        );
+      }
+
+      //NOTIFICACIÓN
+      // Generar notificacion si no es el mismo usuario el que se comenta
+      if (userToNotificate.toString() !== user.toString()) {
+        const notification = new Notification({
+          user: userToNotificate,
+          fromUser: user,
+          targetType: "Comment",
+          targetId: newComment._id,
+        });
+        notification.save({ session });
+
+        await User.findByIdAndUpdate(
+          userToNotificate,
+          {
+            $inc: { unreadNotificationsCount: 1 },
+          },
+          { session },
+        );
+      }
+
+      return res.status(201).json({
+        status: "success",
+        message: "Comentario publicado",
+        comment: newComment,
+      });
     });
   } catch (error) {
     return res.status(500).json({
       status: "error",
       message: "No se pudo publicar el comentario",
+      error,
     });
   }
 };
 
 const remove = async (req, res) => {
-  const user = req.user._id;
   const comment = req.params.id;
 
+  const session = await mongoose.startSession();
+
   try {
-    // Elimina comentario
-    const commentRemoved = await Comment.findOneAndDelete({
-      user,
-      _id: comment,
-    });
+    let commentRemoved;
 
-    if (!commentRemoved) {
-      return res.status(404).json({
-        status: "error",
-        message: "Comentario no encontrado",
+    await session.withTransaction(async () => {
+      // Elimina comentario
+      commentRemoved = await Comment.findByIdAndDelete(comment, { session });
+
+      // CONTADORES
+      if (commentRemoved.parentComment) {
+        // Si tiene comentario padre, le actualiza el contador de respuestas
+        await Comment.findByIdAndUpdate(
+          commentRemoved.parentComment,
+          {
+            $inc: { repliesCount: -1 },
+          },
+          { session },
+        );
+      } else {
+        // Actualiza contador de comentartios de la publicacion
+        await Publication.findByIdAndUpdate(
+          commentRemoved.publication,
+          {
+            $inc: { commentsCount: -1 },
+          },
+          { session },
+        );
+      }
+
+      // Eliminar respuestas
+      await Comment.deleteMany(
+        { parentComment: commentRemoved._id },
+        { session },
+      );
+
+      return res.status(200).json({
+        status: "success",
+        message: "Comentario borrado con éxito",
+        comment: commentRemoved,
       });
-    }
-
-    // CONTADORES
-    // Si tiene comentario padre, le actualiza el contador de respuestas
-    if (commentRemoved.parentComment) {
-      await Comment.findByIdAndUpdate(commentRemoved.parentComment, {
-        $inc: { repliesCount: -1 },
-      });
-    } else {
-      // Actualiza contador de comentartios de la publicacion
-      await Publication.findByIdAndUpdate(commentRemoved.publication, {
-        $inc: { commentsCount: -1 },
-      });
-    }
-
-    // Elimina notificación
-    await Notification.findOneAndDelete({
-      targetType: "Comment",
-      targetId: commentRemoved._id,
-    });
-
-    return res.status(200).json({
-      status: "success",
-      message: "Comentario borrado con éxito",
-      comment: commentRemoved,
     });
   } catch (error) {
     return res.status(500).json({
       status: "error",
-      message: "No se pudo eliminar el comentario",
+      message: "Error en el servidor",
     });
   }
 };
